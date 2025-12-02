ca255ced61b3be98d190d7e9a76696ccb1bbcd6c
Sheetal Gupta <sheetal661999@gmail.com>
2025-12-02 21:10:02 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to our hotel');
@@ -17,6 +20,6 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
     console.log('listening on port 3000')
 })
